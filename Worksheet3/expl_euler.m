1e1786ff78ce77d9be994375487485f5c0abd38d
Matilde Tozzi <ge83bug@tum.de>
2022-12-13 11:51:39 +0100

    finish WS3, need to fix table

--- Worksheet3/expl_euler.m
+++ Worksheet3/expl_euler.m
@@ -1,10 +1,10 @@
 function y = expl_euler(y_0, dt, t_end, f)
 
 t = 0:dt:t_end;
-y = zeros(1, length(t));
-y(1) = y_0;
+y = zeros(length(y_0), length(t));
+y(:,1) = y_0;
 for n = 1:length(t)-1
-    y(n+1) = y(n) + dt * f(y(n));
+    y(:,n+1) = y(:,n) + dt * f(y(:,n));
 end
 
 end
--- Worksheet3/newton.m
+++ Worksheet3/newton.m
@@ -4,8 +4,9 @@ minEps=10e-8;
 i=0;
 epsilon=inf;
 while(i<maxI && epsilon>minEps)
-    x=x_0-G(x_0)./dG(x_0);
-    epsilon=abs(x-x_0)./max(abs(x),1);
+    inv_dG = dG(x_0)\eye(length(x_0));
+    x=x_0-inv_dG*G(x_0);
+    epsilon=norm(x-x_0)./max(norm(x),1);
     x_0=x;
     i=i+1;
 end
