a1fbe24c5094d6392f33ce6a2312b6fbab72c6af
Matilde Tozzi <ge83bug@tum.de>
2023-01-11 16:35:54 +0100

    FIXED

--- Worksheet4/gauss_seidel.m
+++ Worksheet4/gauss_seidel.m
@@ -8,11 +8,11 @@ rnorm = inf;
 N = N_x*N_y;
 x = zeros(N,1);
 j=0;
-while rnorm > 1e-3 && j<1000
+while rnorm > 1e-3
     j=j+1;
     for i=1:N
         sum=0;
-        if(i>N_x+1)
+        if(i>N_x)
         sum=sum+e*x(i-N_x);
         end
         if(i>1 && mod(i,N_x)~=1) 
@@ -21,12 +21,14 @@ while rnorm > 1e-3 && j<1000
         if(i<N&& mod(i,N_x)~=0)
         sum=sum+c*x(i+1);
         end
-        if(i<N-N_x-1)
+        if(i<N-N_x)
         sum=sum+e*x(i+N_x);
         end
         x(i) = 1/d*(b(i)-sum);
     end
     rnorm=residual_norm(N_x, N_y, b, x);
 end
+j
+rnorm
 x = reshape(x, N_x, N_y);
 end
--- Worksheet4/residual_norm.m
+++ Worksheet4/residual_norm.m
@@ -1,3 +1,31 @@
 function r = residual_norm(N_x, N_y, b, x)
-r = inf;
+
+h2_x = (1/(N_x+1))^2;
+h2_y = (1/(N_y+1))^2;
+d = -2/h2_x-2/h2_y;
+c = 1/h2_y;
+e = 1/h2_x;
+N = N_x*N_y;
+g = zeros(N,1);
+
+for i=1:N
+    s=0;
+    if(i>N_x)
+        s=s+e*x(i-N_x);
+    end
+    if(i>1 && mod(i,N_x)~=1)
+        s=s+c*x(i-1);
+    end
+    if(i<N&& mod(i,N_x)~=0)
+        s=s+c*x(i+1);
+    end
+    if(i<N-N_x)
+        s=s+e*x(i+N_x);
+    end
+    s = s+d*x(i);
+    g(i) = (b(i)-s)^2;
+end
+
+r = sqrt(1/N*sum(g,'all'));
+
 end
